fd0269cb593dde2422a764aa2465cbec4d097158
Bajlur Rahaman <bajlurrahaman2@gmail.com>
2022-06-13 20:50:13 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Oct 01, 2022 00:00:00').getTime(),
+    x = setInterval(function() {    
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, second);
+
+    $('.owl-gallery').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:true,
+      responsive:{
+          0:{
+              items:2
+          },
+          420:{
+              items:3
+          },
+          600:{
+              items:5
+          },
+          1000:{
+              items:5
+          }
+      }
+  });
+
+  $('.owl-team').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
